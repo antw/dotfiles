73214e4da26dfcd5142425a962d2ea7fb1988bf5
Anthony Williams <hi@antw.dev>
2021-11-20 20:45:41 +0000

    ZSH: Add kubectl aliases only when installed

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -2,7 +2,9 @@ alias be="bundle exec"
 alias ls="exa"
 alias z="fasd_cd -d"
 
-alias k=kubectl
+if command -v kubectl; then
+  alias k=kubectl
 
-source <(kubectl completion zsh)
-complete -o default -F __start_kubectl k
+  source <(kubectl completion zsh)
+  complete -o default -F __start_kubectl k
+fi

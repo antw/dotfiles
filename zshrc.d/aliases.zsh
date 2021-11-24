ed32e473e5fd4037f71da95099764e9dc9fd6953
Anthony Williams <hi@antw.dev>
2021-11-24 15:05:44 +0000

    ZSH: Remove kubectl completion
    ... another ZSH startup performance hog.

--- zshrc.d/aliases.zsh
+++ zshrc.d/aliases.zsh
@@ -1,10 +1,3 @@
 alias be="bundle exec"
 alias ls="exa"
 alias z="fasd_cd -d"
-
-if command -v kubectl; then
-  alias k=kubectl
-
-  source <(kubectl completion zsh)
-  complete -o default -F __start_kubectl k
-fi
